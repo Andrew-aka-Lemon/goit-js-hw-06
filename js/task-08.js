bf7c4f599f51f063f3e5c847ca744a82964e933c
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-09-09 18:31:35 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const inputRef = document.querySelector('.login-form');
 
+let formData = {};
+
+inputRef.addEventListener('submit', dataCollector);
+
+function dataCollector(event) {
+  event.preventDefault();
+
+  const userPass = event.currentTarget.elements.password.value;
+  const userLogin = event.currentTarget.elements.email.value;
+
+  if (userLogin === '' || userPass === '') {
+    window.alert('Всі поля повинні бути заповнені');
+  }
+
+  formData = {
+    userLogin,
+    userPass,
+  };
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
