b2b5c5c2e8e5058b26ef2bd6e4ad3f4d9672538d
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-09-09 22:39:27 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const textOfColorRef = document.querySelector('.color');
+const changeColorBtnRef = document.querySelector('.change-color');
+
+function changeBodyColor() {
+  const newColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = newColor;
+  textOfColorRef.textContent = newColor;
+}
+
+changeColorBtnRef.addEventListener('click', changeBodyColor);
